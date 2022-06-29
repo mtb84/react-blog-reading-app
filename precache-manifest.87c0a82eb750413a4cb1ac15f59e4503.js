2016484f41a70b2bf410c45c8a9aafe0a6dbad3d
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-29 15:13:46 -0400

    Updates

---
+++ precache-manifest.87c0a82eb750413a4cb1ac15f59e4503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19e9fd24805f57905ecc",
+    "url": "/react-blog-reading-app/static/css/main.13855fbd.chunk.css"
+  },
+  {
+    "revision": "19e9fd24805f57905ecc",
+    "url": "/react-blog-reading-app/static/js/main.19e9fd24.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-blog-reading-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/react-blog-reading-app/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "6735e2072d5435ffc50f",
+    "url": "/react-blog-reading-app/static/js/runtime~main.6735e207.js"
+  },
+  {
+    "revision": "27078d2b763b194177daf1e494261cfb",
+    "url": "/react-blog-reading-app/index.html"
+  }
+];
